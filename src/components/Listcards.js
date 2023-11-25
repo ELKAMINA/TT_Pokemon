4c277f32f706b620deadaebe7fd39d3bb7a7b980
EL KHATTABI <elkhattabi.amina@gmail.com>
2023-11-25 14:24:52 +0100

    Ok infinite scroll

--- src/components/Listcards.js
+++ src/components/Listcards.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { CircularProgress, Box, Container, Typography } from "@mui/material";
 import Card from "./Card";
 import useCards from "../hooks/useCards";
@@ -7,32 +8,53 @@ const Listcards = () => {
  const [page, setPage] = useState(1);
  const { results, isLoading, isError, error, hasNextPage } = useCards(page);
 
- const intObserver = useRef();
+ const lastCardInPost = useRef();
+ const fetchMoreCards = () => {
+  if (hasNextPage) {
+   setPage((prev) => prev + 1);
+  }
+ };
 
  if (isError) return <Typography>{error.message}</Typography>;
 
- const content = results.map((card, i) => {
-  console.log("card ", card);
-  if (results.length === i + 1) {
-   return (
-    <Card ref={intObserver} key={card.id} card={card}>
-     {card.name}
-    </Card>
-   );
-  }
-  return <Card key={card.id} card={card}></Card>;
- });
+ //  const content = results.map((card, i) => {
+ //   console.log("card ", card);
+ //   if (results.length === i + 1) {
+ //    return (
+ //     <Card ref={lastCardInPost} key={card.id} card={card}>
+ //      {card.name}
+ //     </Card>
+ //    );
+ //   }
+ //   return <Card key={card.id} card={card}></Card>;
+ //  });
 
  return (
   <Container>
    <Box>
-    {content}
-    {isLoading && (
-     <>
-      <Typography> More data is loading ... </Typography>
-      <CircularProgress />
-     </>
-    )}
+    {/* {content} */}
+    <InfiniteScroll
+     dataLength={results.length} // to let InfiniteScroll know how many items are already rendered
+     next={fetchMoreCards} // fetches next data. Contains previous and next data
+     hasMore={true}
+     loader={
+      isLoading && (
+       <>
+        <Typography> More data is loading ... </Typography>
+        <CircularProgress />
+       </>
+      )
+     }
+     endMessage={<Typography> You've seen all card available ... </Typography>}
+    >
+     {results?.map((pokemon, index) => (
+      <Card key={pokemon.id} card={pokemon}></Card>
+     ))}
+    </InfiniteScroll>
+    <Typography>
+     {" "}
+     <a href="#top"> Back to top</a>
+    </Typography>
    </Box>
   </Container>
  );
