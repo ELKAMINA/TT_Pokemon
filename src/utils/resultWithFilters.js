e1f3ffa0b5fc13ab630cf8b8ef416001640a94ff
EL KHATTABI <elkhattabi.amina@gmail.com>
2023-11-27 00:39:21 +0100

    Ajout Filtres pour prix

--- src/utils/resultWithFilters.js
+++ src/utils/resultWithFilters.js
@@ -11,10 +11,26 @@ export const getResultWithFilters = (results, filters) => {
     results = results.filter((result) => result.rarity === "Rare Holo");
     break;
    case "Descending prices":
-    // results = results.sort((a, b) => b.price[0].low - a.price[0].low);
+    results = results.sort((a, b) => {
+     const aPrice = a.cardmarket?.prices?.averageSellPrice
+      ? pricing(a.cardmarket?.prices?.averageSellPrice).toFixed(3)
+      : 1;
+     const bPrice = b.cardmarket?.prices?.averageSellPrice
+      ? pricing(b.cardmarket?.prices?.averageSellPrice).toFixed(3)
+      : 1;
+     return bPrice - aPrice;
+    });
     break;
    case "Ascending prices":
-    // results = results.sort((a, b) => a.price[0].low - b.price[0].low);
+    results = results.sort((a, b) => {
+     const aPrice = a.cardmarket?.prices?.averageSellPrice
+      ? pricing(a.cardmarket?.prices?.averageSellPrice).toFixed(3)
+      : 1;
+     const bPrice = b.cardmarket?.prices?.averageSellPrice
+      ? pricing(b.cardmarket?.prices?.averageSellPrice).toFixed(3)
+      : 1;
+     return aPrice - bPrice;
+    });
     break;
    default:
     break;
