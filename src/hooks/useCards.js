41f5e5a9487e7261c10ea0be5112b79058a11e1b
EL KHATTABI <elkhattabi.amina@gmail.com>
2023-11-28 23:18:03 +0100

    Revue de code: useCards Ok

--- src/hooks/useCards.js
+++ src/hooks/useCards.js
@@ -1,27 +1,22 @@
-import { useState, useEffect, useRef } from "react";
-import { getAllCardsPage } from "../services/requests";
-import { useAppSelector, useAppDispatch } from "../redux/hooks/hooks";
 import {
  selectSearchquery,
  selectFilters,
  selectPage,
- setFilters,
- setSearchquery,
  setLoading,
  resetPage,
- selectLoading,
 } from "../redux/slices/pokemonSlice";
-import { getResultWithFilters } from "../utils/resultWithFilters";
+import { useState, useEffect, useRef } from "react";
 import { compareArrays } from "../utils/arrays";
+import { getAllCardsPage } from "../services/requests";
+import { getResultWithFilters } from "../utils/resultWithFilters";
+import { useAppSelector, useAppDispatch } from "../redux/hooks/hooks";
 
 const useCards = () => {
- const [results, setResults] = useState([]);
- //  const [isLoading, setLoading] = useState(false);
- const [isError, setIsError] = useState(false);
- const [error, setError] = useState([]);
- const [hasNextPage, setHasNextPage] = useState(false);
  const dispatch = useAppDispatch();
-
+ const [error, setError] = useState([]);
+ const [results, setResults] = useState([]);
+ const [isError, setIsError] = useState(false);
+ const [hasNextPage, setHasNextPage] = useState(false);
  const query = useAppSelector(selectSearchquery);
  const page = useAppSelector(selectPage);
  const lastQuery = useRef([]);
@@ -30,6 +25,7 @@ const useCards = () => {
  const abortControllerRef = useRef(null);
 
  useEffect(() => {
+  /* If 2 requests collide, abort the first one */
   if (!abortControllerRef.current) {
    abortControllerRef.current = new AbortController();
   } else {
@@ -37,76 +33,65 @@ const useCards = () => {
    abortControllerRef.current = new AbortController();
   }
   const signal = abortControllerRef.current.signal;
+  /* ***** */
 
-  //   const abortController = new AbortController();
-  //   const signal = abortController.signal;
-  //   console.log("USECARD filter = ", filters);
+  /* Resetting states when comp re-render */
   dispatch(setLoading(true));
   setIsError(false);
   setError({});
-  console.log("ici 1 ");
-  /* ***** */
-  //   console.log("lastqury", lastQuery.current);
-  //   console.log("query", query);
-  //   console.log("filters.length ", filters.length);
-  //   console.log("lastFilters.current ", lastFilter.current.length);
+
+  /* Know if continuation of data with pagination or new req */
   const isNewQuery = compareArrays(lastQuery.current, query) ? false : true;
   const isNewFilter = filters.length !== lastFilter.current.length;
-  console.log("ici 2 ");
   if (isNewQuery || isNewFilter || (isNewQuery && isNewFilter)) {
+   /* If new query or new filter, reset page & result array*/
    setResults([]);
    dispatch(resetPage());
-   console.log("ici 3 ");
   }
+
+  /* Fetching data */
   getAllCardsPage(page, 50, query, filters, {
    signal: abortControllerRef.current.signal,
   })
    .then((data) => {
-    console.log("ici 6 ");
+    /* If request is not aborted, do */
     if (!signal.aborted) {
      setHasNextPage(Boolean(data?.length)); // Recall : 0 is false
      if (query.length > 0 || filters.length > 0) {
-      console.log("ici 7 ");
-
       if (data?.length < 50) setHasNextPage(false);
      }
      setResults((prev) => {
       if (!prev) return;
-      console.log("ici 8 ");
-
       const updatedResults = isNewQuery ? data : [...prev, ...data];
-      console.log("finalResults", updatedResults);
       const finalResults = getResultWithFilters(updatedResults, filters);
       return finalResults;
      });
-
      setLoading(false);
      dispatch(setLoading(false));
     }
-    // console.log("Fetched Data:", data.length);
    })
    .catch((error) => {
-    console.log("ici ?", error);
     setIsError(true);
     setError(error);
     setLoading(false);
     dispatch(setLoading(false));
     if (error.name === "CanceledError" && signal.aborted) {
+     console.error("Fetch aborted");
      setResults([]);
      dispatch(resetPage());
      dispatch(setLoading(true));
      setIsError(false);
      setError({});
      setHasNextPage(false);
-     console.log("Fetch aborted");
      return; // Early return for aborted requests
     }
-    // if (signal.aborted) return; // If the request is aborted, don't update the state bc error created on purpose
    });
 
+  /* Keeping track of last query and filters to compare with new ones */
   lastQuery.current = query;
   lastFilter.current = filters;
 
+  /* Unmounting, abort the previous request */
   return () => {
    abortControllerRef.current.abort();
   };
